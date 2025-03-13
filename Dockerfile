bffb0b85066e5bbb1038e379ca864ce68f4ed7ba
Luma <rdslum@gmail.com>
2025-03-13 18:55:09 -0300

    Configuring docker and env variables

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,12 @@ WORKDIR /app
 
 COPY requirements.txt /app/
 
+COPY .env /app/
+
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . /app/
+
 ENV PYTHONUNBUFFERED 1
 
 EXPOSE 8000
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Product, Category, Brand, CustomerReview
 from .serializers import ProductSerializer, CategorySerializer, BrandSerializer, CustomerReviewSerializer
 
@@ -6,6 +9,26 @@ from .serializers import ProductSerializer, CategorySerializer, BrandSerializer,
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    @action(detail=True, methods=['patch'])
+    def remove_from_stock(self, request, pk=None):
+        try:
+            product = self.get_object()
+            quantity = request.data.get('quantity', 0)
+            if quantity <= 0:
+                return Response({"error": "Quantity must be greater than 0."}, status=status.HTTP_400_BAD_REQUEST)
+
+            if quantity > product.stock:
+                return Response({"error": "Insufficient stock."}, status=status.HTTP_400_BAD_REQUEST)
+
+            product.stock -= quantity
+            product.save()
+
+            serializer = self.get_serializer(product)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        except Product.DoesNotExist:
+            return Response({"error": "Product not found."}, status=status.HTTP_404_NOT_FOUND)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
